0f8724a0b66723a57d836a021dfe20933214c7a6
Jordan Last <jordan.michael.last@gmail.com>
2018-06-08 00:09:53 -0600

    switch between math topics and objectives

--- elements/ghana-math-jhs-1.js
+++ elements/ghana-math-jhs-1.js
@@ -16,7 +16,60 @@ const InitialState = {
             '1.2.2 describe and write sets of objects and numbers'
         ]
     },
-    selectedObjectives: []
+    activities: {
+        '1.1.1 Count and write numerals up to 100,000,000': [
+            'Guide pupils to revise counting and writing numerals in ten thousands, hundred thousands and millions.',
+            'Using the idea of counting in millions, guide pupils to recognize the number of millions in ten million as (10,000,000 = 10 1,000,000)',
+            'Using the non-proportional structured materials like the abacus or colour-coded materials, guide pupils to count in ten millions.',
+            'Show, for example, 54,621,242 on a place value chart.',
+            'Point out that the commas between periods make it easier to read numerals.',
+            'Assist pupils to read number names of given numerals (E.g. 54,621,242) as; Fifty four million, six hundred and twenty one thousand, two hundred and forty two.',
+            'Other'
+        ],
+        '1.1.2 identify and explain the place values of  digits in a numeral up to 100,000,000': [
+            'Using the abacus or place value chart guide pupils to find the place value of digits in numerals up to 8-digits.',
+            'Discuss with pupils the value of digits in given numerals. E.g. in 27,430,561 the value of 6 is 60, the value of 3 is 30,000, the value of 7 is 7,000,000, etc',
+            'Discus with pupils the difference between the place value of a digit in a numeral and the value of a digit in a numeral.'
+        ],
+        '1.2.1 identify sets of objects and numbers': [
+            'Guide pupils to collect and sort objects into groups and let pupils describe the sets of objects formed',
+            'Guide pupils to form other sets(groups) according to a given criteria using objects and numbers',
+            'Introduce the concept of a set as a well defined collection of objects or ideas',
+            'Guide pupils to use real life situations to form sets. E.g. a set of prefects in the school'
+        ],
+        '1.2.2 describe and write sets of objects and numbers': [
+            '"Introduce ways of describing and writing sets using: Defining property; i.e. describing the members (elements) of a set in words. E.g. a set of mathematical instruments. Listing the members of a set using only curly brackets„{ }‟ and commas to separate the members. E.g. S = {0, 1, 2,…, 26}',
+            'NOTE: Use capital letters to represent sets. E.g. A = {months of the year}."'
+        ]
+
+    },
+    materials: {
+        '1.1.1 Count and write numerals up to 100,000,000': [
+            'Abacus',
+            'Colour-coded materials',
+            'Place value chart',
+            'Other'
+        ],
+        '1.1.2 identify and explain the place values of  digits in a numeral up to 100,000,000': [],
+        '1.2.1 identify sets of objects and numbers': [],
+        '1.2.2 describe and write sets of objects and numbers': []
+    },
+    evaluations: {
+        '1.1.1 Count and write numerals up to 100,000,000': [
+            'read and write number names and numerals as teacher calls out the digits in a given numeral (E.g. 72,034,856)',
+            'bring in news papers or magazines that mention numbers in millions to record)',
+            'mention numbers they hear on TV and radio reports (this can be taken as projects to be carried out weekly for pupils;',
+            'investigate types of numbers that appear in government‟s budgets, elections results, census reports, etc.',
+            'Other'
+        ],
+        '1.1.2 identify and explain the place values of  digits in a numeral up to 100,000,000': [],
+        '1.2.1 identify sets of objects and numbers': [],
+        '1.2.2 describe and write sets of objects and numbers': []
+    },
+    selectedObjectives: [],
+    selectedActivities: [],
+    selectedMaterials: [],
+    selectedEvaluations: []
 };
 
 const RootReducer = (state = InitialState, action) => {
@@ -55,6 +108,26 @@ class GhanaMathJHS1 extends HTMLElement {
         });
     }
 
+    objectivesSelectChange(e) {
+        Store.dispatch({
+            type: 'SET_LOCAL_STATE',
+            key: 'selectedActivities',
+            value: Store.getState().activities[e.target.value]
+        });
+
+        Store.dispatch({
+            type: 'SET_LOCAL_STATE',
+            key: 'selectedMaterials',
+            value: Store.getState().materials[e.target.value]
+        });
+
+        Store.dispatch({
+            type: 'SET_LOCAL_STATE',
+            key: 'selectedEvaluations',
+            value: Store.getState().evaluations[e.target.value]
+        });
+    }
+
     render(state) {
         return html`
             <h1>Math JHS 1 Lesson Notes</h1>
@@ -87,11 +160,11 @@ class GhanaMathJHS1 extends HTMLElement {
                 </select>
             </div>
 
-            <div>
+            <div hidden="${state.selectedObjectives.length === 0}">
                 <div>
                     Objectives
                 </div>
-                <select>
+                <select onchange="${(e) => this.objectivesSelectChange(e)}">
                     <option selected disabled hidden>Choose</option>
                     ${state.selectedObjectives.map((objective) => {
                         return html`
@@ -99,6 +172,50 @@ class GhanaMathJHS1 extends HTMLElement {
                         `;
                     })}
                 </select>
+            </div>
+
+            <div hidden="${state.selectedActivities.length === 0}">
+                <div>
+                    Activities
+                </div>
+                <div>
+                    ${state.selectedActivities.map((activity) => {
+                        return html`
+                            <div>
+                                <input type="checkbox"> ${activity} ${activity === 'Other' ? html`<input type="text">` : ''}
+                            </div>
+                        `;
+                    })}
+                </div>
+            </div>
+
+            <div hidden="${state.selectedObjectives.length === 0 || state.selectedActivities.length === 0}">
+                <div>
+                    Core Points
+                </div>
+                <input type="text">
+            </div>
+
+            <div hidden="${state.selectedMaterials.length === 0}">
+                <div>
+                    Materials
+                </div>
+                <div>
+                    ${state.selectedMaterials.map((material) => {
+                        return html`
+                            <div>
+                                <input type="checkbox"> ${material} ${material === 'Other' ? html`<input type="text">` : ''}
+                            </div>
+                        `;
+                    })}
+                </div>
+            </div>
+
+            <div hidden="${state.selectedObjectives.length === 0 || state.selectedActivities.length === 0}">
+                <div>
+                    Additional Notes
+                </div>
+                <input type="text">
             </div>
         `;
     }
